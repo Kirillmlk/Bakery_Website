7c138d26ab60cb755065e8cc179f5e395705ed16
Kirillmlk <kostakov.kiirill@yandex.by>
2024-12-04 23:20:02 +0300

    bug fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -28,14 +28,14 @@ const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 
-if (navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
-if (navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -44,41 +44,42 @@ if (navClose){
 
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-const blurHeader = () =>{
+const blurHeader = () => {
     const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
+    // Заменили this.scrollY на window.scrollY
+    window.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
 
 
-const scrollUp = () =>{
+const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up')
-    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-        : scrollUp.classList.remove('show-scroll')
+    // Заменили this.scrollY на window.scrollY
+    window.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
 const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () =>{
+const scrollActive = () => {
     const scrollDown = window.scrollY
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
             sectionTop = current.offsetTop - 58,
             sectionId = current.getAttribute('id'),
             sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
             sectionsClass.classList.add('active-link')
-        }else{
+        } else {
             sectionsClass.classList.remove('active-link')
         }
     })
@@ -94,19 +95,18 @@ const sr = ScrollReveal({
     duration: 2500,
     delay: 300,
     // reset: true,
-
 })
 
 sr.reveal(`.home__data, .about__img, .about__data, .visit__data`)
 
-sr.reveal(`.home__image, .footer__img-1, .footer__img-2`, { rotate: {z: -15} })
-sr.reveal(`.home__bread, .about__bread`, { rotate: {z: 15} })
+sr.reveal(`.home__image, .footer__img-1, .footer__img-2`, { rotate: { z: -15 } })
+sr.reveal(`.home__bread, .about__bread`, { rotate: { z: 15 } })
 sr.reveal(`.home__footer`, { scale: 1, origin: 'bottom' })
 
-sr.reveal(`.new__card:nth-child(1) img`, { rotate: {z: -30}, distance: 0 })
-sr.reveal(`.new__card:nth-child(2) img`, { rotate: {z: 15}, distance: 0, delay: 600 })
-sr.reveal(`.new__card:nth-child(3) img`, { rotate: {z: -30}, distance: 0, delay: 900 })
+sr.reveal(`.new__card:nth-child(1) img`, { rotate: { z: -30 }, distance: 0 })
+sr.reveal(`.new__card:nth-child(2) img`, { rotate: { z: 15 }, distance: 0, delay: 600 })
+sr.reveal(`.new__card:nth-child(3) img`, { rotate: { z: -30 }, distance: 0, delay: 900 })
 
-sr.reveal(`.favorite__card img`, { interval: 100, rotate: {z: 15}, distance: 0 })
+sr.reveal(`.favorite__card img`, { interval: 100, rotate: { z: 15 }, distance: 0 })
 
 sr.reveal(`.footer__container`, { scale: 1 })
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', '') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.7.0/remixicon.css">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.7.0/remixicon.css">
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/css/app.css','resources/js/app.js',  "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- ScrollReveal CDN -->
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/css/app.css','resources/js/app.js',  "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+<body class="font-sans antialiased">
+@inertia
+</body>
 </html>
