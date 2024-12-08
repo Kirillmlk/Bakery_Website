ec53374aa1b2e25cf124a63957bda4d1b13d0f13
Kirillmlk <kostakov.kiirill@yandex.by>
2024-12-08 22:53:49 +0300

    Post viewing page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Post\StoreRequest;
+use App\Http\Resources\Post\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,14 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
+
+        $posts = PostResource::collection($posts)->resolve();
         return inertia('Post/Index', compact('posts'));
+    }
+
+    public function show(Post $post)
+    {
+        return inertia('Post/Show', compact('post'));
     }
 
     public function create()
